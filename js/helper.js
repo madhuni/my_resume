fcdc2a904de41ce250ed7eaf0832b703049eea04
madhuni <kanishkamohanmadhuni@gmail.com>
2017-01-16 13:38:40 +0000

    Helping variables have been defined for the "card-1" and it's different sections.

---
+++ js/helper.js
@@ -0,0 +1,17 @@
+var HTMLresumePic = '<figure class="flex-box"><img src=%data% alt="Profile Picture"></figure>';
+var HTMLresumeTitle = '<h1 class="resume-title">%data%</h1>';
+
+// we will keep the #bio-container in the HTML file.
+
+/* Variables for the "bio-section" starts here */
+// we will keep the "#bio-section" as well in the HTML file.
+var HTMLcontactContainer = '<div class="details"></div>';
+var HTMLbioContent = '<i class="fa %font-awesome-class%"></i><span>%data%</span>';
+
+// we will keep the "#skills" section in the HTML file.
+var HTMLskillsContainer = '<div class="details flex-box"></div';
+var HTMLskillContent = '<img src="./img/web-tech-logo/%logo-image%" alt="HTML5 logo" width="25" height="25" class="tech-logo"><span>%data%</span>';
+
+// we will keep the "#lang-section" in the HTML file.
+var HTMLlangContainer = '<div class="details"></div>';
+var HTMLlangContent = '<span>%data%</span><progress value="%cur-value%" max="100"></progress>';

717c02b041513c30899cc6c7e56848b366d11800
madhuni <kanishkamohanmadhuni@gmail.com>
2017-01-16 17:18:34 +0000

    Code for the "displayLanguage" has been implemented.

--- js/resume_builder.js
+++ js/resume_builder.js
@@ -2,14 +2,11 @@
 
 var bio = {
 	"name" : "Kanishka Mohan Madhuni",
-
 	"role" : {
 		"roleDesc" : "Front-End Web Developer",
 		"roleLogo" : "fa-briefcase"
 	},
-
 	"profilePicUrl" : "./img/my-profile-pic-2.jpg",
-
 	"contactInfo" : {
 		"contactType" : {
             "location" : "Bangalore, India",
@@ -20,26 +17,24 @@ var bio = {
         },
 		"contactInfoLogo" : ["fa-map-marker", "fa-envelope", "fa-phone", "fa-github-square", "fa-paperclip"]
 	},
-
 	"skills" : {
-		"skillLogoUrl" : ["./img/web-tech-logo/html5.svg", 
-			"./img/web-tech-logo/css3.svg", 
-			"./img/web-tech-logo/js.svg", 
-			"./img/web-tech-logo/jquery.svg", 
+		"skillLogoUrl" : ["./img/web-tech-logo/html5.svg",
+			"./img/web-tech-logo/css3.svg",
+			"./img/web-tech-logo/js.svg",
+			"./img/web-tech-logo/jquery.svg",
 			"./img/web-tech-logo/bootstrap.svg"],
 		"skillDesc" : ["HTML-5", "CSS-3", "Javascript", "JQuery", "Bootstrap"]
 	},
-
 	"language" : {
 		"langDesc" : ["English", "Hindi"],
-		"langPercent" : ["85%", "90%"]
+		"langPercent" : ["85", "90"]
 	}
-
 };
 
 /* Code for desplaying BIO starts here */
 
 function displayTitle() {
+    
     var formattedTitle = HTMLresumeTitle.replace("%data%", bio.name);
     var formattedPic = HTMLresumePic.replace("%data%", bio.profilePicUrl);
      
@@ -66,7 +61,7 @@ function displayContact() {
 };
 
 function displaySkills() {
-    
+
     for (var i=0; i<bio.skills.skillDesc.length; i++) {
         var logoUrl = bio.skills.skillLogoUrl[i];
         var skillDesc = bio.skills.skillDesc[i];
@@ -78,6 +73,20 @@ function displaySkills() {
     }
 };
 
+function displayLanguage() {
+    
+	for (var i=0; i<bio.language.langDesc.length; i++) {
+        var lang = bio.language.langDesc[i];
+        var percent = bio.language.langPercent[i];
+        
+        var formattedLang = HTMLlangContent.replace("%data%",lang).replace("%cur-value%", percent);
+        
+        $("#lang-section:last").append(HTMLlangContainer);
+        $(".details:last").append(formattedLang);
+    }
+};
+
 displayTitle();
 displayContact();
 displaySkills();
+displayLanguage();

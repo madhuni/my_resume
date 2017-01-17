64f4c809a7484e13b78b517c2e2a35e585af2f3c
madhuni <kanishkamohanmadhuni@gmail.com>
2017-01-17 15:24:13 +0000

    Helper js file has been updated with the helper items for work-experience section.

--- js/helper.js
+++ js/helper.js
@@ -18,3 +18,15 @@ var HTMLskillContent = '<img src="%logo-image%" alt="HTML5 logo" width="25" heig
 // we will keep the "#lang-section" in the HTML file.
 var HTMLlangContainer = '<div class="details"></div>';
 var HTMLlangContent = '<span>%data%</span><progress value="%cur-value%" max="100"></progress>';
+
+
+/* Templet for the work-experience section starts here */
+// we will keep the "#work-experience" section in the HTML.
+// Section heading will also be there in the HTML.
+var HTMLworkArticle = '<article id="work-article" class="container"><div id="job-items" class="flex-box"></div></article>';
+var HTMLjobDescription = '<p class="job-description">%data%</p>';
+var HTMLcompanyLogo = '<div class="company-logo"><img src="./img/cognizant_logo.png" alt="Company Logo" width="45" height="45"></div>';
+var HTMLjobDescItems = '<div class="job-desc-items"></div>';
+var HTMLcompanyAndRole = '<h4 class="article-heading">"%data%" / "%data%"';
+var HTMLjobDuration = '<div class="job-duration"><i class="fa  fa-calendar"></i><span class="duration">%data%</span></div>';
+var HTMLjobLocation = '<div class="job-location"><i class="fa fa-map-marker"></i><span class="location-description">%data%</span></div>';

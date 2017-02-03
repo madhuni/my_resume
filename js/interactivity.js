028292d68537f9af86316c2198fc80a1666beeb6
madhuni <kanishkamohanmadhuni@gmail.com>
2017-02-03 02:45:01 +0000

    New code for applying media-queries from the Javascript has been implemented.

--- js/interactivity.js
+++ js/interactivity.js
@@ -2,56 +2,52 @@ $(function($) {
     
     /* Creating function for toggling the sections in the section-container */
     function toggleSections(parent) {
-
         var tapTarget = parent.children('.section-heading');
         var toggleTarget = parent.children('article');
         var toggleTarget2 = parent.find('.sub-section');
         var hrRule = parent.find('hr');
 
         tapTarget.click(function(evt) {
+            // console.log("toggleSections function is working");
+            // console.log(tapTarget);
+            // console.log(toggleTarget);
+            // console.log(toggleTarget2);
+            // console.log(hrRule);
             toggleTarget.slideToggle();
             toggleTarget2.slideToggle();
             hrRule.slideToggle();
         });
-    };
-    var projectParent = $('#projects');
-    var jobParent = $('#work-experience');
-    var eduParent = $('#educaiton');
-    
-    // Initializing the function
-    toggleSections(jobParent);
-    toggleSections(projectParent);
-    toggleSections(eduParent);
-    
-    
-    /* Writing code for automatic toggle of the sections below some view-port size */
-    var viewportWidth = $(window).width();
-   // console.log(viewportWidth);
-    
-    /*function checkSize() {
-        
-        var containerWidth = $('main').width(); //numeric value
-        var sectionWidth = $('#section-container').width(); //numaric value
-       // console.log(containerWidth);
-       // console.log(sectionWidth);
-        var jobParent = $('#work-experience');
+    }
 
-        if(sectionWidth === containerWidth-20) {
-            console.log("I am in the if section");
-            jobParent.children('.work-article').addClass('hidden');
-            jobParent.children('hr').addClass('hidden');
-            toggleSections(jobParent);
-        } else {
-            console.log("I am in else section");
-            jobParent.children('.work-article').removeClass('hidden');
-            jobParent.children('hr').removeClass('hidden');
-           // stopToggleSection(jobParent);
-        }
-    };
+    function stopToggling(parent) {
+        console.log("stopToggling function is working");
+        var tapTarget = parent.children('.section-heading');
+        tapTarget.click(function(evt) {
+            evt.stopPropagation();
+        });
+    }
     
+/* Writing code for automatic toggle of the sections below some view-port size */
+    function checkSize() {
+        var projectParent = $('#projects');
+        var jobParent = $('#work-experience');
+        var eduParent = $('#educaiton');
+
+        if (window.matchMedia('(min-width : 920px)').matches) {
+            // console.log("i am in the if condition");
+            stopToggling(projectParent);
+            stopToggling(jobParent);
+            stopToggling(eduParent);
+        } else {
+            // console.log("I am in the else condition");
+            toggleSections(jobParent);
+            toggleSections(projectParent);
+            toggleSections(eduParent);
+        }
+    }
+
     checkSize();
-    console.log("rezie funtion is running here");
-    $(window).resize(checkSize);*/
+    $(window).resize(checkSize);
 
 
 /* Creating function for displaying hidden details in the articles */ 

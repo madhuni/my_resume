d4835f6a47a84b4ff69b8f11c10a5425c4e5f8da
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-07-22 01:44:01 +0530

    the model have been changed as per the rubric sheet

--- js/resume_builder.js
+++ js/resume_builder.js
@@ -1,49 +1,101 @@
 /* Making the Bio-obeject for the Resume */
 
 var bio = {
-    "name": "Kanishka Mohan Madhuni",
-    "role": {
-        "roleDesc": "Front-End Web Developer",
-        "roleLogo": "fa-briefcase"
+    name: "Kanishka Mohan Madhuni",
+    role: "Front-End Web Developer",
+    contacts: {
+        location: "Bangalore, India",
+        email: "kmmadhuni@gmail.com",
+        mobile: "+91-8954054974",
+        github: "https://github.com/madhuni/",
+        portfolio: "https://madhuni.github.io/"
     },
-    "aboutMe": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-    "profilePicUrl": "./img/my-profile-pic-2.jpg",
-    "contactInfo": {
-        "contactType": {
-            "location": "Bangalore, India",
-            "email": "kmmadhuni@gmail.com",
-            "mobile": "+91-8954054974",
-            "gitHub": "https://github.com/madhuni/",
-            "portfolio": "https://madhuni.github.io/"
-        },
-        "contactInfoLogo": ["fa-map-marker", "fa-envelope", "fa-phone", "fa-github-square", "fa-paperclip"]
-    },
-    "skills": {
-        "skillLogoUrl": ["./img/web-tech-logo/html5.svg",
+    welcomeMessage: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    skills: ["HTML-5", "CSS-3", "Javascript", "JQuery", "Bootstrap"],
+    biopic: "./img/my-profile-pic-2.jpg",
+    extraDetails : {
+        contactInfoLogo: ["fa-map-marker", "fa-envelope", "fa-phone", "fa-github-square", "fa-paperclip"],
+        skillLogoUrl: ["./img/web-tech-logo/html5.svg",
             "./img/web-tech-logo/css3.svg",
             "./img/web-tech-logo/js.svg",
             "./img/web-tech-logo/jquery.svg",
             "./img/web-tech-logo/bootstrap.svg"
         ],
-        "skillDesc": ["HTML-5", "CSS-3", "Javascript", "JQuery", "Bootstrap"]
-    },
-    "language": {
-        "langDesc": ["English", "Hindi"],
-        "langPercent": ["85", "90"]
+        language: {
+            "langDesc": ["English", "Hindi"],
+            "langPercent": ["85", "90"]
+        }
     }
+};
+
+bio.display = function () {
+    displayTitle();
+    displayContact();
+    displaySkills();
+    displayLanguage();
+    displayAboutMe();
+};
+
+/* Code for desplaying BIO starts here */
+
+function displayTitle() {
+    var formattedTitle = HTMLresumeTitle.replace("%data%", bio.name);
+    var formattedPic = HTMLresumePic.replace("%data%", bio.biopic);
+    $("#card-1").prepend(formattedTitle);
+    $("#card-1").prepend(formattedPic);
+};
+
+function displayContact() {
+    var contacts = [];
+    $.each(bio.contacts, function(key, value){
+        contacts.push(value);
+    });    
+    // console.log(contacts);
+    for (var i=0; i<contacts.length; i++) {
+        var formattedFaClass = bio.extraDetails.contactInfoLogo[i];
+        var formattedContactType = contacts[i];
+        var formattedContect = HTMLcontactContent.replace("%font-awesome-class%",formattedFaClass).replace("%data%", formattedContactType);
+        $("#contact-section:last").append(HTMLcontactContainer);
+        $(".details:last").append(formattedContect);
+    }
+};
+
+function displaySkills() {
+    for (var i=0; i<bio.skills.length; i++) {
+        var logoUrl = bio.extraDetails.skillLogoUrl[i];
+        var skillDesc = bio.skills[i];
+        var formattedSkillContent = HTMLskillContent.replace("%logo-image%", logoUrl).replace("%data%", skillDesc);
+        $("#skills:last").append(HTMLskillsContainer);
+        $(".details:last").append(formattedSkillContent);
+    }
+};
+
+function displayLanguage() {
+    for (var i=0; i<bio.extraDetails.language.langDesc.length; i++) {
+        var lang = bio.extraDetails.language.langDesc[i];
+        var percent = bio.extraDetails.language.langPercent[i];
+        var formattedLang = HTMLlangContent.replace("%data%",lang).replace("%cur-value%", percent);
+        $("#lang-section:last").append(HTMLlangContainer);
+        $(".details:last").append(formattedLang);
+    }
+};
+
+function displayAboutMe() {
+    var formattedAboutMe = HTMLaboutMe.replace('%data%', bio.welcomeMessage);
+    $("#about-me-section:last").append(formattedAboutMe);
 };
 
 /* Work-experience object goes here */
 
 var work = {
-    "jobs": [{
-        "employer": "Cognizant Technology Solutions",
-        "title": "Application Analyst",
-        "locaiton": "Chennai/Bangalore",
-        "dates": "April 2015 - Present",
-        "description": "I have worked as a Application Analyst in the EDI. EDI which stands for Electronic Data Interchange is a third party team which the client use to exchange the different kind of files in different formates to their business partners. I had to moinitor the applicaton so that the flow of flies will remain fine and do the automation for cutting down the time in day to day operations.",
-        "companyLogo": "./img/cognizant_logo.png",
-        "experience": "1 Year, 9 Months"
+    jobs: [{
+        employer: "Cognizant Technology Solutions",
+        title: "Application Analyst",
+        locaiton: "Chennai/Bangalore",
+        dates: "April 2015 - Present",
+        description: "I have worked as a Application Analyst in the EDI. EDI which stands for Electronic Data Interchange is a third party team which the client use to exchange the different kind of files in different formates to their business partners. I had to moinitor the applicaton so that the flow of flies will remain fine and do the automation for cutting down the time in day to day operations.",
+        companyLogo: "./img/cognizant_logo.png",
+        experience: "1 Year, 9 Months"
     }]
 };
 
@@ -76,23 +128,23 @@ work.display = function() {
 
 /* Featured Project Obeject Goes Here */
 
-var featuredProjects = {
-    "projects": [{
-        "title": "My Portfolio Project",
-        "dates": "December 2016",
-        "description": "This is my portfolio Project. I have used HMTL5 for markup and CSS3 for the styling of the project. The page has been made fully responsive to work across all the devices. Bootstrap is also used to give the interactivity to the page. The CSS Flexbox layout has been used to design the entire layout of the page.",
-        "images": "http://lorempixel.com/1200/720"
-    }, {
-        "title": "My Interactive Resume",
-        "dates": "Jan 2016",
-        "description": "I have made this iteractive resume to showcase my details on an online plateform. This resume has been built up using HTML5 and CSS3 flex box layout working across all the browsers. JQuery has been used to manipulate the DOM elements and for more features.",
-        "images": "http://lorempixel.com/1200/720"
-    }]
-};
 
-featuredProjects.display = function() {
+var projects = [{
+    title: "My Portfolio Project",
+    dates: "December 2016",
+    description: "This is my portfolio Project. I have used HMTL5 for markup and CSS3 for the styling of the project. The page has been made fully responsive to work across all the devices. Bootstrap is also used to give the interactivity to the page. The CSS Flexbox layout has been used to design the entire layout of the page.",
+    images: "http://lorempixel.com/1200/720"
+}, {
+    title: "My Interactive Resume",
+    dates: "Jan 2016",
+    description: "I have made this iteractive resume to showcase my details on an online plateform. This resume has been built up using HTML5 and CSS3 flex box layout working across all the browsers. JQuery has been used to manipulate the DOM elements and for more features.",
+    images: "http://lorempixel.com/1200/720"
+}];
+
+
+projects.display = function() {
     
-    var items = featuredProjects.projects;
+    var items = projects;
      
     for (var i=0; i<items.length; i++) {
         
@@ -115,104 +167,43 @@ featuredProjects.display = function() {
 /* Obeject for the Education Goes Here */
 
 var education = {
-    "onlineEducation": [{
-        "plateform": "Udacity",
-        "duration": "December 2016 - Present",
-        "certificationTitle": "Front End Development Nanodegree"
+    schools: [{
+        name: "Moradabad Institute of Technology",
+        location: "Moradabad, Uttar Pradesh",
+        degree: "B.Tech.",
+        majors: "Mechanical Engineering",
+        dates: "2010-2014"
+    }, {
+        name: "Chitragupt Inter College",
+        location: "Moradabad, Uttar Pradesh",
+        degree: "Intermediate",
+        majors: "Physics, Chemistry, Maths",
+        dates: "2007-2009"
+    }, {
+        name: "Chitragupt Inter College",
+        location: "Moradabad, Uttar Pradesh",
+        degree: "Highschool",
+        majors: "Physics, Chemistry, Maths",
+        dates: "2006-2007"
     }],
-    "academics": [{
-        "collegeName": "Moradabad Institute of Technology",
-        "degree": "B.Tech.",
-        "duration": "2010-2014",
-        "location": "Moradabad, Uttar Pradesh",
-        "major": "Mechanical Engineering"
-    }, {
-        "collegeName": "Chitragupt Inter College",
-        "degree": "Intermediate",
-        "duration": "2007-2009",
-        "location": "Moradabad, Uttar Pradesh",
-        "major": "Physics, Chemistry, Maths"
-    }, {
-        "collegeName": "Chitragupt Inter College",
-        "degree": "Highschool",
-        "duration": "2006-2007",
-        "location": "Moradabad, Uttar Pradesh",
-        "major": "Physics, Chemistry, Maths"
+    onlineCourses: [{
+        title: "Front End Development Nanodegree",
+        school: "Udacity",
+        dates: "December 2016 - Present",
+        url : "https://udacity.com"
     }]
 };
 
-/* Code for desplaying BIO starts here */
-
-function displayTitle() {
-    
-    var formattedTitle = HTMLresumeTitle.replace("%data%", bio.name);
-    var formattedPic = HTMLresumePic.replace("%data%", bio.profilePicUrl);
-	$("#card-1").prepend(formattedTitle);
-    $("#card-1").prepend(formattedPic);
-};
-
-function displayContact() {
-
-    var contacts = [];
-    
-    $.each(bio.contactInfo.contactType, function(key, value){
-        contacts.push(value);
-    });    
-    // console.log(contacts);
-    for (var i=0; i<contacts.length; i++) {
-
-        var formattedFaClass = bio.contactInfo.contactInfoLogo[i];
-        var formattedContactType = contacts[i];
-        
-        var formattedContect = HTMLcontactContent.replace("%font-awesome-class%",formattedFaClass).replace("%data%", formattedContactType);
-        $("#contact-section:last").append(HTMLcontactContainer);
-        $(".details:last").append(formattedContect);
-    }
-};
-
-function displaySkills() {
-
-    for (var i=0; i<bio.skills.skillDesc.length; i++) {
-        var logoUrl = bio.skills.skillLogoUrl[i];
-        var skillDesc = bio.skills.skillDesc[i];
-        
-        var formattedSkillContent = HTMLskillContent.replace("%logo-image%", logoUrl).replace("%data%", skillDesc);
-        
-        $("#skills:last").append(HTMLskillsContainer);
-        $(".details:last").append(formattedSkillContent);
-    }
-};
-
-function displayLanguage() {
-    
-	for (var i=0; i<bio.language.langDesc.length; i++) {
-        var lang = bio.language.langDesc[i];
-        var percent = bio.language.langPercent[i];
-        
-        var formattedLang = HTMLlangContent.replace("%data%",lang).replace("%cur-value%", percent);
-        
-        $("#lang-section:last").append(HTMLlangContainer);
-        $(".details:last").append(formattedLang);
-    }
-};
-
-function displayAboutMe() {
-
-    var formattedAboutMe = HTMLaboutMe.replace('%data%', bio.aboutMe);
-    $("#about-me-section:last").append(formattedAboutMe);
-
-};
-
-function dispalyEducation() {
+education.display = function () {
     // displaying the Online education first
-    var eduType1 = education.onlineEducation;
-    var eduType2 = education.academics;
+    var eduType1 = education.onlineCourses;
+    var eduType2 = education.schools;
     
     $.each(eduType1, function(index, value) {
-        var formattedArticleHeading = HTMLarticleHeading1.replace("%data%",eduType1[index].plateform);
+        var formattedArticleHeading = HTMLarticleHeading1.replace("%data%",eduType1[index].school);
         /*console.log(formattedArticleHeading);*/
-        var formattedEduDuration = HTMLeduDuration1.replace("%data%", eduType1[index].duration);
-        var formattedMajor = HTMLmajor1.replace("%data%", eduType1[index].certificationTitle);
+        var formattedEduDuration = HTMLeduDuration1.replace("%data%", eduType1[index].dates);
+        var formattedMajor = HTMLmajor1.replace("%data%", eduType1[index].title);
         
         $("#online-education:last").append(HTMLonlineEduArticle);
         $(".online-edu-article:last").append(formattedArticleHeading);
@@ -222,10 +213,10 @@ function dispalyEducation() {
     });
     
     $.each(eduType2, function(index, value) {
-        var formattedArticleHeading = HTMLarticleHeading2.replace("%data%",eduType2[index].collegeName).replace("%data%",eduType2[index].degree);
-        var formattedEduDuration = HTMLeduDuration2.replace("%data%", eduType2[index].duration);
+        var formattedArticleHeading = HTMLarticleHeading2.replace("%data%",eduType2[index].name).replace("%data%",eduType2[index].degree);
+        var formattedEduDuration = HTMLeduDuration2.replace("%data%", eduType2[index].dates);
         var formattedLocation = HTMLeduLocation.replace("%data%", eduType2[index].location);
-        var formattedMajor = HTMLmajor2.replace("%data%", eduType2[index].major);
+        var formattedMajor = HTMLmajor2.replace("%data%", eduType2[index].majors);
         
         $("#academic-education:last").append(HTMLacademicEduArticle);
         $(".academic-edu-article:last").append(formattedArticleHeading);
@@ -242,13 +233,8 @@ function dispalyEducation() {
 
 //here we are using the "failsafe" jQuery
 jQuery(function($) {
-    displayTitle();
-    displayContact();
-    displaySkills();
-    displayLanguage();
-    displayAboutMe();
+    bio.display();
     work.display();
-    featuredProjects.display();
-    dispalyEducation();
+    projects.display();
+    education.display();
 });
-
